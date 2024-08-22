4f4b3a037eff19e99d3163a07ca64514c3bc9a87
jazzdev1010 <suchismitamaharana2000@gmail.com>
2024-08-23 01:05:37 +0900

    Modified path in misslabel_dataset.py

--- ML/code/misslabel_dataset.py
+++ ML/code/misslabel_dataset.py
@@ -64,8 +64,11 @@ def main(argv):
         dataset = argv[0]
         filename = dataset + argv[1]
         dir = "data/" + dataset + "/"
+        
+        npy_filename = os.path.join('../ML/', dir + filename + '.npy')
+        print(npy_filename)
 
-        data = np.load(os.path.join('../ML', dir + filename + '.npy'))
+        data = np.load(npy_filename)
         data[:, -1] += int(argv[2]) - int(argv[1])
         
         save_file = dir + dataset + "_bad_" + argv[1] + "_" + argv[2]
